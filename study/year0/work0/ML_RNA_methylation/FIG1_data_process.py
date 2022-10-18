5d53bcb868d675767a0e55bd3cfb4f364c03c6c4
jt <912126472@qq.com>
2022-10-18 17:12:39 +0800

    fig1 data process

--- study/year0/work0/ML_RNA_methylation/FIG1_data_process.py
+++ study/year0/work0/ML_RNA_methylation/FIG1_data_process.py
@@ -1,7 +1,9 @@
 import pandas
 import os
 source_dir = 'RMT_results'
+classifiers = ["GB","GNB","LR","RF","SVM"]
 draw_dirs = os.listdir(source_dir)
+draw_dirs = [d for d in draw_dirs if d.__contains__("draw_")]
 rank_nums = len(draw_dirs)
 for draw_dir in draw_dirs:
-    
+    rank_num = draw_dir.split("_")[-1]
