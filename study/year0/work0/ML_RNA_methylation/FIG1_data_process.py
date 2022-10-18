4554cab6ca8796995929c796c67e480b100deb15
jt <912126472@qq.com>
2022-10-18 22:51:56 +0800

    fig1 data process 80%

--- study/year0/work0/ML_RNA_methylation/FIG1_data_process.py
+++ study/year0/work0/ML_RNA_methylation/FIG1_data_process.py
@@ -1,9 +1,31 @@
-import pandas
 import os
+import pandas
 source_dir = 'RMT_results'
-classifiers = ["GB","GNB","LR","RF","SVM"]
+classifiers = ["GB", "GNB", "LR", "RF", "SVM"]
+data_out_dir = 'data'
+data_out_name = 'FIG1_data.txt'
 draw_dirs = os.listdir(source_dir)
 draw_dirs = [d for d in draw_dirs if d.__contains__("draw_")]
 rank_nums = len(draw_dirs)
-for draw_dir in draw_dirs:
-    rank_num = draw_dir.split("_")[-1]
+data_frames = []
+for classifier in classifiers:
+    data_frame = None
+    for draw_dir in draw_dirs:
+        rank_num = draw_dir.split("_")[-1]
+        file_path = os.path.join(source_dir, draw_dir, f'{classifier}_pr{rank_num}_all_genes.csv')
+        tmp = pandas.read_csv(file_path, delimiter=',', index_col=0, low_memory=False)
+        if data_frame is not None:
+            data_frame += tmp
+        else:
+            data_frame = tmp
+    data_frame /= rank_nums
+    data_frame.iloc[:, 0] = classifier
+    data_frame.rename(columns={'0':'Classifer','1':'value'})
+    data_frames.append(data_frame)
+
+with open(os.path.join(data_out_dir, data_out_name),'w') as f:
+    data_frames.values
+    for data_frame in data_frames:
+        v = data_frame.values
+        f.write(v)
+f.close()
