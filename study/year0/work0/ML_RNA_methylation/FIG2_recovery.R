ca950dbb6a55a76abe6a58e98cf97113ac79ba1a
jt <912126472@qq.com>
2022-10-24 01:42:31 +0800

    FIG2_recovery80%

--- study/year0/work0/ML_RNA_methylation/FIG2_recovery.R
+++ study/year0/work0/ML_RNA_methylation/FIG2_recovery.R
@@ -1,14 +1,15 @@
 setwd("C:\\USC\\study\\year0\\work0\\ML_RNA_methylation")
 library(ggplot2)
 library(reshape2)
-data <- read.csv("data\\FIG2_REPORT.TXT",sep = "\t")
-data = head(data,30)
-x = data$Fold.Enrichment
-y = factor(data$Term,levels = data$Term)
-
+data <- read.csv("data\\FIG2_REPORT_fix.csv",sep = "\t")
+x = data$Classifier
+y = factor(data$Term,levels = unique(data$Term))
 p = ggplot(data,aes(x,y))
+
+#color = -log10(PValue)
 
 p1 = p + geom_point(aes(size = Count,color=p.adjust(PValue,method = "BH"))) + 
    scale_color_gradient(low = "Red",high = "Blue") + 
+   theme_bw() +
    labs(x="",y="",color="p.adjust")
 p1
