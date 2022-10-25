631bae5916c3fef4285a24d2963813556139a0e9
jt <912126472@qq.com>
2022-10-25 23:00:21 +0800

    fig2 fix

--- study/year0/work0/ML_RNA_methylation/FIG2_report_process.py
+++ study/year0/work0/ML_RNA_methylation/FIG2_report_process.py
@@ -6,9 +6,9 @@ report_datas = []
 for report_dir in os.listdir(src_dir):
     report = pandas.read_csv(os.path.join(src_dir, report_dir), sep="\t")
     report.sort_values("Count", ascending=False, inplace=True)
-    report = report.head(30)
+    report = report.head(20)
     report["Term"] = [t.split("~")[-1] for t in report["Term"]]
-    report["Classifier"] = report_dir.split("_")[0]
+    report["Classifier"] = report_dir.split(".")[0]
     report_datas.append(report)
 report = pandas.concat(report_datas)
 report.to_csv(os.path.join("data", data_out_name), sep='\t', index=False)
