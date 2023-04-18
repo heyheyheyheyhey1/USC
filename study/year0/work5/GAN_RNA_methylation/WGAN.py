d409a5e47d00f6d962be227f53959e47e5ff8416
jt <912126472@qq.com>
2023-04-19 02:08:56 +0800

    WGAN

--- study/year0/work5/GAN_RNA_methylation/WGAN.py
+++ study/year0/work5/GAN_RNA_methylation/WGAN.py
@@ -4,22 +4,21 @@ from torch import nn
 import pickle
 import numpy as np
 import pandas as pd
-
+import tqdm
 
 
 class Discriminator(nn.Module):
-    def __init__(self, p):
+    def __init__(self, in_dim):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(p.in_dim, 8192),
-            nn.Linear(8192, 10240),
-            nn.Linear(10240, 8192),
-            nn.Linear(8192, p.in_dim),
-            nn.Linear(p.in_dim, 2048),
-            nn.Linear(2048,1024),
-            nn.Linear(1024,512),
-            nn.Linear(512,256),
-            nn.Linear(256,1),
+            nn.Linear(in_dim, 2048),
+            nn.Linear(2048, 4096),
+            nn.Linear(4096, 4096),
+            nn.Linear(4096, 2048),
+            nn.Linear(2048, in_dim),
+            nn.Linear(in_dim,512),
+            nn.Linear(512, 256),
+            nn.Linear(256, 1),
         )
 
     def forward(self, x):
@@ -28,26 +27,69 @@ class Discriminator(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self,p):
+    def __init__(self, in_dim,out_dim):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(p.in_dim,8192),
-            nn.Linear(8192,8192),
-            nn.Linear(8192,8192),
-            nn.Linear(8192,8192),
-            nn.Linear(8192,8192),
-            nn.Linear(8192,p.out_dim)
+            nn.Linear(in_dim, 2048),
+            nn.Linear(2048, 2048),
+            nn.Linear(2048, 2048),
+            nn.Linear(2048, out_dim),
+            nn.Tanh()
         )
 
     def forward(self, x):
         out = self.model(x)
         return out
 
+
 class WGAN():
-    def __init__(self,args):
-        self.train_data = args.train_data,
-        self.arg_g = args.arg_g
-        self.arg_d = args.arg_d
-        pass
+    def __init__(self, args):
+        self.train_data = args["train_data"]
+        self.train_opt = args["train_opt"]
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        arg_g = {"in_dim": self.train_data.shape[1], "out_dim": self.train_data.shape[1]}
+        arg_d = {"in_dim": self.train_data.shape[1]}
+        self.generator = Generator(**arg_g).to(self.device)
+        self.discriminator = Discriminator(**arg_d).to(self.device)
+        self.optimizer_G = torch.optim.RMSprop(self.generator.parameters(), lr=self.train_opt.lr)
+        self.optimizer_D = torch.optim.RMSprop(self.discriminator.parameters(), lr=self.train_opt.lr)
+        self.losses_D = []
+        self.losses_G = []
+
+    def train_data_enumerate(self):
+        for i in range(int(len(self.train_data) / self.train_opt.batch_size)):
+            data_i = self.train_data[i * self.train_opt.batch_size: (i + 1) * self.train_opt.batch_size]
+            yield torch.Tensor(data_i).to(self.device)
+
     def train(self):
-        pass
+        for i in tqdm.tqdm(range(self.train_opt.n_epochs)):
+            self.train_one_epoch(i + 1)
+
+    def train_one_epoch(self, n_epoch):
+        for real_data in self.train_data_enumerate():
+            # discriminator training
+            for j in range(self.train_opt.n_critic):
+                self.optimizer_D.zero_grad()
+                # random noise
+                noise = torch.randn(real_data.shape).to(self.device)
+                # generator fake data through random noise
+                fake_data = self.generator(noise).detach()
+                # loss
+                loss_D = -torch.mean(self.discriminator(real_data)) + torch.mean(self.discriminator(fake_data))
+                loss_D.backward()
+                self.optimizer_D.step()
+                for params in self.discriminator.parameters():
+                    params.data.clamp_(-self.train_opt.clip_value, self.train_opt.clip_value)
+                self.losses_D.append(loss_D)
+            # generator training
+            self.optimizer_G.zero_grad()
+            noise = torch.randn(real_data.shape).to(self.device)
+            gen_data = self.generator(noise)
+            loss_G = -torch.mean(self.discriminator(gen_data))
+            loss_G.backward()
+            self.optimizer_G.step()
+            self.losses_G.append(loss_G)
+        print ("\n[Epoch %d/%d] [D loss: %f] [G loss: %f]" % (n_epoch, self.train_opt.n_epochs,
+                                                            torch.mean(torch.FloatTensor(self.losses_D)),
+                                                            torch.mean(torch.FloatTensor(self.losses_G))))
+
--- study/year0/work5/GAN_RNA_methylation/train.py
+++ study/year0/work5/GAN_RNA_methylation/train.py
@@ -5,22 +5,33 @@ import pickle
 import numpy as np
 import pandas as pd
 from WGAN import WGAN
+import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--n_epochs', type=int, default=1800, help='number of epochs of training')
-parser.add_argument('--batch_size', type=int, default=16, help='size of the batches')
-parser.add_argument('--lr', type=float, default=0.00005, help='learning rate')
-parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
-parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
-parser.add_argument('--feature_size', type=int, default=934, help='size of each image dimension')
-parser.add_argument('--channels', type=int, default=1, help='number of image channels')
-parser.add_argument('--n_critic', type=int, default=5, help='number of training steps for discriminator per iter')
-parser.add_argument('--clip_value', type=float, default=0.01, help='lower and upper clip value for disc. weights')
-parser.add_argument('--sample_interval', type=int, default=400, help='interval betwen image samples')
-opt = parser.parse_args()
+DATA_DIR = os.path.join("data")
+
 
 def main():
-    wgan = WGAN()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_epochs', type=int, default=1800, help='number of epochs of training')
+    parser.add_argument('--batch_size', type=int, default=10, help='size of the batches')
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    parser.add_argument('--feature_size', type=int, default=934, help='size of each image dimension')
+    parser.add_argument('--n_critic', type=int, default=5, help='number of training steps for discriminator per iter')
+    parser.add_argument('--clip_value', type=float, default=0.01, help='lower and upper clip value for disc. weights')
+    parser.add_argument('--sample_interval', type=int, default=400, help='interval betwen image samples')
+    opt = parser.parse_args()
+    selected_data = "selected_dataset.tsv"
+    rnmts = "RNMT.list"
+    dataset_matrix = os.path.join(DATA_DIR, selected_data)
+    selected_data = pd.read_csv(dataset_matrix, delimiter="\t", index_col=0, low_memory=False)
+    positive_genes = [line.rstrip('\n') for line in open(os.path.join(DATA_DIR, rnmts))]
+    positive_data = selected_data.loc[positive_genes].values
+    args = {}
+    args["train_data"] = positive_data
+    args["train_opt"] = opt
+    wgan = WGAN(args)
     wgan.train()
+
+
 if __name__ == "__main__":
     main()
