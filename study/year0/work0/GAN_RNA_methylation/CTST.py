5bc6393c681574998a8651ac08a2afa4f6c14553
jt <912126472@qq.com>
2023-04-26 01:28:32 +0800

    TSNE VISUALIZE

--- study/year0/work0/GAN_RNA_methylation/CTST.py
+++ study/year0/work0/GAN_RNA_methylation/CTST.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import cross_val_score, cross_validate, LeaveOneOut
 from sklearn.metrics import make_scorer, accuracy_score, precision_score
 from sklearn.utils import shuffle
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
 DATA_DIR = os.path.join("data")
 MODEL_DIR = os.path.join("model")
@@ -21,15 +23,15 @@ selected_data = pd.read_csv(dataset_matrix, delimiter="\t", index_col=0, low_mem
 positive_genes = [line.rstrip('\n') for line in open(os.path.join(DATA_DIR, rnmts))]
 positive_data = selected_data.loc[positive_genes].values
 # 生成数据
-negative_data = generator(torch.rand([92, 128]))
+synthetic_data = generator(torch.rand([92, 128]))
 # 保存数据
-pd.DataFrame(negative_data.detach().numpy()).to_csv(os.path.join(DATA_DIR, "synthetic_data.csv"))
+pd.DataFrame(synthetic_data.detach().numpy()).to_csv(os.path.join(DATA_DIR, "synthetic_data.csv"))
 y0 = np.zeros([92, ])
 y1 = np.ones([92, ])
 #定义x
 y = np.concatenate([y0, y1], axis=0)
 #定义y
-x = np.concatenate([negative_data.detach(), positive_data])
+x = np.concatenate([synthetic_data.detach(), positive_data])
 #打乱
 x,y = shuffle(x,y)
 #定义svm
@@ -42,5 +44,15 @@ scorings = {'accuracy': make_scorer(accuracy_score),
 cv_result = cross_validate(SVM_model,x,y,scoring=scorings,cv=5)
 print(cv_result["test_accuracy"].mean())
 print(cv_result["test_precision"].mean())
+
+tsne = TSNE(n_components=2, learning_rate=500).fit_transform(x)
+plt.scatter(tsne[y==1,0], tsne[y==1,1],c="red" ,label = "real data")
+plt.scatter(tsne[y==0,0], tsne[y==0,1],c="blue" ,label = "GAN synthetic data")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend(loc=4)
+plt.grid(True)
+plt.tight_layout()
+plt.savefig(os.path.join("visualize_syn_real.png"))
 
 
---
+++ study/year0/work0/GAN_RNA_methylation/visualize.py
@@ -0,0 +1,66 @@
+import os
+import random
+
+import pandas as pd
+import torch
+from WGAN import Generator
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score, cross_validate, LeaveOneOut
+from sklearn.metrics import make_scorer, accuracy_score, precision_score
+from sklearn.utils import shuffle
+from imblearn.over_sampling import *
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+DATA_DIR = os.path.join("data")
+MODEL_DIR = os.path.join("model")
+SAMPLE_NUMS = 92
+generator = Generator(in_dim=128, out_dim=1517)
+generator.load_state_dict(torch.load(os.path.join(MODEL_DIR, "generator", "generator_n_20000.pth")))
+generator.eval()
+smoter = SMOTE(random_state=42)
+
+selected_data = "selected_dataset.tsv"
+rnmts = "RNMT.list"
+dataset_matrix = os.path.join(DATA_DIR, selected_data)
+selected_data = pd.read_csv(dataset_matrix, delimiter="\t", index_col=0, low_memory=False)
+all_genes = selected_data.index.unique().to_list()
+positive_genes = [line.rstrip('\n') for line in open(os.path.join(DATA_DIR, rnmts))]
+positive_genes = random.sample(positive_genes, SAMPLE_NUMS)
+negative_genes = set(all_genes).difference(positive_genes)
+# 真实正样本数据
+positive_data = selected_data.loc[positive_genes].values
+# 真实负样本数据
+negative_genes = random.sample(negative_genes, SAMPLE_NUMS)
+negative_data = selected_data.loc[negative_genes].values
+# GAN生成数据
+GAN_data = generator(torch.rand([SAMPLE_NUMS*2, 128]))
+
+y0 = np.zeros([SAMPLE_NUMS*2, ])
+y1 = np.ones([SAMPLE_NUMS, ])
+
+
+# 定义x
+y = np.concatenate([y0, y1], axis=0)
+# 定义y
+x = np.concatenate([GAN_data.detach(), positive_data])
+# SMOTE生成数据
+x,y = smoter.fit_resample(x,y)
+#去掉gan数据
+x = x[SAMPLE_NUMS-1:-1]
+y = y[SAMPLE_NUMS-1:-1]
+y[2*SAMPLE_NUMS-1:-1] = 2.
+
+
+
+tsne2d = TSNE(n_components=2, learning_rate=100).fit_transform(x)
+plt.scatter(tsne2d[y == 0, 0], tsne2d[y == 0, 1], c="blue", label="GAN data")
+plt.scatter(tsne2d[y == 1, 0], tsne2d[y == 1, 1], c="red", label="positive data")
+plt.scatter(tsne2d[y == 2, 0], tsne2d[y == 2, 1], c="black", label="SMOTE data")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend(loc="best")
+plt.grid(True)
+plt.tight_layout()
+plt.savefig(os.path.join("visualize_gan_smote_postv.png"))
