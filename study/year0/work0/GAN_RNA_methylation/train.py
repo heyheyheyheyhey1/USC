54b5c6bcf48752a785ece5ffea46d2b7b2cbf975
jt <912126472@qq.com>
2023-05-03 01:50:05 +0800

    wgan gp

--- study/year0/work0/GAN_RNA_methylation/train.py
+++ study/year0/work0/GAN_RNA_methylation/train.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 import pandas as pd
 from WGAN import WGAN
+from WGANGP import WGANGP
 import os
 
 DATA_DIR = os.path.join("data")
@@ -22,9 +23,12 @@ def main():
     parser.add_argument('--n_epochs', type=int, default=20000, help='number of epochs of training')
     parser.add_argument('--batch_size', type=int, default=8, help='size of the batches')
     parser.add_argument('--lr', type=float, default=0.00005, help='learning rate')
-    parser.add_argument('--n_critic', type=int, default=3, help='number of training steps for discriminator per iter')
+    parser.add_argument('--n_critic', type=int, default=5, help='number of training steps for discriminator per iter')
     parser.add_argument('--clip_value', type=float, default=0.01, help='lower and upper clip value for disc. weights')
     parser.add_argument('--latent_dim', type=int, default=128, help='dimensionality of the latent space')
+    parser.add_argument('--beta1', type=int, default=0, help='wgangp optimizer parameter')
+    parser.add_argument('--beta2', type=int, default=0.9, help='wgangp optimizer parameter')
+    parser.add_argument('--r', type=int, default=10, help='define gradient penalty factor')
     opt = parser.parse_args()
     selected_data = "selected_dataset.tsv"
     rnmts = "RNMT.list"
@@ -37,7 +41,7 @@ def main():
     args["train_opt"] = opt
     args["g_sav_dir"] = MODEL_DIR_G
     args["d_sav_dir"] = MODEL_DIR_D
-    wgan = WGAN(args)
+    wgan = WGANGP(args)
     wgan.train()
 
 
