2ed7da53da18feeb4a1a762aefd8778d8f3dbca9
jt <912126472@qq.com>
2023-07-05 22:33:20 +0800

    update WGANGP C2ST

--- study/year0/work0/GAN_RNA_methylation/CTST.py
+++ study/year0/work0/GAN_RNA_methylation/CTST.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import torch
-from WGAN import Generator
+from WGANGP import Generator
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score, cross_validate, LeaveOneOut
 from sklearn.metrics import make_scorer, accuracy_score, precision_score
@@ -9,50 +9,93 @@ from sklearn.utils import shuffle
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
+from tqdm import tqdm
 
 DATA_DIR = os.path.join("data")
-MODEL_DIR = os.path.join("model")
+MODEL_DIR = os.path.join("model", "wgangp")
 generator = Generator(in_dim=128, out_dim=1517)
-generator.load_state_dict(torch.load(os.path.join(MODEL_DIR, "generator", "generator_n_20000.pth")))
-generator.eval()
 
 selected_data = "selected_dataset.tsv"
-rnmts = "RNMT.list"
+rnmts = "test_positive_genes.txt"
 dataset_matrix = os.path.join(DATA_DIR, selected_data)
 selected_data = pd.read_csv(dataset_matrix, delimiter="\t", index_col=0, low_memory=False)
 positive_genes = [line.rstrip('\n') for line in open(os.path.join(DATA_DIR, rnmts))]
 positive_data = selected_data.loc[positive_genes].values
+mdls = os.listdir(os.path.join(MODEL_DIR, "generator"))
+acc_average = pd.DataFrame(index=mdls)
+
+
+def weight_reset(m):
+    if hasattr(m, 'reset_parameters'):
+        m.reset_parameters()
+
+
+# c2st 测试
+for mdl in tqdm(mdls):
+    generator.apply(weight_reset)
+    generator.load_state_dict(torch.load(os.path.join(MODEL_DIR, "generator", mdl)))
+    generator.eval()
+
+    SVM_model = SVC()
+    scorings = {'accuracy': make_scorer(accuracy_score)}
+    for i in range(20):
+        # 生成数据
+        synthetic_data = generator(torch.rand([19, 128]))
+        x = np.concatenate([synthetic_data.detach(), positive_data])
+
+        # 定义y
+        y0 = np.zeros([19, ])
+        y1 = np.ones([19, ])
+        y = np.concatenate([y0, y1], axis=0)
+        x, y = shuffle(x, y)
+
+        cv_result = cross_validate(SVM_model, x, y, scoring=scorings, cv=3)
+        acc_average.loc[mdl, f'round_{i}'] = cv_result["test_accuracy"].mean()
+
+    acc_average.loc[mdl, "mean"] = acc_average.loc[mdl].mean().round(5)
+    acc_average.loc[mdl, "var"] = acc_average.loc[mdl].var().round(5)
+    acc_average.loc[mdl, "threshold"] = abs(acc_average.loc[mdl].mean() - 0.5).round(5)
+
+# 导出
+acc_average.sort_values("threshold", ascending=True ,inplace=True)
+acc_average.to_csv(os.path.join(MODEL_DIR, "CTST_result.csv"), index=True)
+
+# 载入最佳模型
+generator.apply(weight_reset)
+selected_mdl = acc_average.index[0]
+generator.load_state_dict(torch.load(os.path.join(MODEL_DIR, "generator", selected_mdl)))
+generator.eval()
+
 # 生成数据
-synthetic_data = generator(torch.rand([92, 128]))
+synthetic_data = generator(torch.rand([19, 128]))
 # 保存数据
-pd.DataFrame(synthetic_data.detach().numpy()).to_csv(os.path.join(DATA_DIR, "synthetic_data.csv"))
-y0 = np.zeros([92, ])
-y1 = np.ones([92, ])
-#定义x
+# pd.DataFrame(synthetic_data.detach().numpy()).to_csv(os.path.join(DATA_DIR, "synthetic_data.csv"))
+y0 = np.zeros([19, ])
+y1 = np.ones([19, ])
+# 定义x
 y = np.concatenate([y0, y1], axis=0)
-#定义y
+# 定义y
 x = np.concatenate([synthetic_data.detach(), positive_data])
-#打乱
-x,y = shuffle(x,y)
-#定义svm
+# 打乱
+x, y = shuffle(x, y)
+# 定义svm
 SVM_model = SVC()
 loocv = LeaveOneOut()
 scorings = {'accuracy': make_scorer(accuracy_score),
             'precision': make_scorer(precision_score),
             }
 # SVM_model.fit(x,y)
-cv_result = cross_validate(SVM_model,x,y,scoring=scorings,cv=5)
+cv_result = cross_validate(SVM_model, x, y, scoring=scorings, cv=3)
 print(cv_result["test_accuracy"].mean())
 print(cv_result["test_precision"].mean())
 
-tsne = TSNE(n_components=2, learning_rate=500).fit_transform(x)
-plt.scatter(tsne[y==1,0], tsne[y==1,1],c="red" ,label = "real data")
-plt.scatter(tsne[y==0,0], tsne[y==0,1],c="blue" ,label = "GAN synthetic data")
+tsne = TSNE(n_components=2, random_state=42, learning_rate=200, init="pca").fit_transform(x)
+plt.scatter(tsne[y == 1, 0], tsne[y == 1, 1], c="red", label="real data")
+plt.scatter(tsne[y == 0, 0], tsne[y == 0, 1], c="blue", label="GAN synthetic data")
 plt.xlabel('x')
 plt.ylabel('y')
 plt.legend(loc=4)
 plt.grid(True)
 plt.tight_layout()
 plt.savefig(os.path.join("visualize_syn_real.png"))
-
-
+plt.show()
--- study/year0/work0/GAN_RNA_methylation/train.py
+++ study/year0/work0/GAN_RNA_methylation/train.py
@@ -6,7 +6,9 @@ import numpy as np
 import pandas as pd
 from WGAN import WGAN
 from WGANGP import WGANGP
+from random import sample
 import os
+
 
 DATA_DIR = os.path.join("data")
 MODEL_DIR = os.path.join("model","wgangp")
@@ -20,25 +22,47 @@ if not os.path.exists(MODEL_DIR_D):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--n_epochs', type=int, default=20000, help='number of epochs of training')
+    parser.add_argument('--n_epochs', type=int, default=1500, help='number of epochs of training')
     parser.add_argument('--batch_size', type=int, default=12, help='size of the batches')
     parser.add_argument('--lr', type=float, default=0.00005, help='learning rate')
-    parser.add_argument('--lr_g', type=float, default=0.00005, help='learning rate g')
-    parser.add_argument('--lr_d', type=float, default=0.00002, help='learning rate d')
+    parser.add_argument('--lr_g', type=float, default=0.00001, help='learning rate g')
+    parser.add_argument('--lr_d', type=float, default=0.00001, help='learning rate d')
     parser.add_argument('--n_critic', type=int, default=7, help='number of training steps for discriminator per iter')
     parser.add_argument('--clip_value', type=float, default=0.01, help='lower and upper clip value for disc. weights')
     parser.add_argument('--latent_dim', type=int, default=128, help='dimensionality of the latent space')
     parser.add_argument('--beta1', type=int, default=0.9, help='wgangp optimizer parameter')
     parser.add_argument('--beta2', type=int, default=0.95, help='wgangp optimizer parameter')
     parser.add_argument('--r', type=int, default=5, help='define gradient penalty factor')
+    parser.add_argument('--stop_threshold', type=int, default=0.05, help='early stop c2st threshold')
     parser.add_argument('--type', type=str, default="gp", help='GAN type')
     opt = parser.parse_args()
+
     selected_data = "selected_dataset.tsv"
     rnmts = "RNMT.list"
+
     dataset_matrix = os.path.join(DATA_DIR, selected_data)
     selected_data = pd.read_csv(dataset_matrix, delimiter="\t", index_col=0, low_memory=False)
     positive_genes = [line.rstrip('\n') for line in open(os.path.join(DATA_DIR, rnmts))]
-    positive_data = selected_data.loc[positive_genes].values
+    unlabel_genes = list(selected_data.index.difference(positive_genes))
+
+    train_positive_genes = sample(positive_genes, int(len(positive_genes) * 0.8))
+    train_unlable_genes = sample(unlabel_genes, int(len(unlabel_genes) * 0.8))
+
+    positive_data = selected_data.loc[train_positive_genes].values
+    # unlabel_data = selected_data.loc[train_unlable_genes].values
+
+    ft = open(os.path.join(DATA_DIR, "test_positive_genes.txt"), "w")
+    fu = open(os.path.join(DATA_DIR, "test_negative_genes.txt"), "w")
+
+    for gene in set(positive_genes).difference(train_positive_genes):
+        ft.write(gene)
+        ft.write("\n")
+    ft.close()
+    for gene in set(unlabel_genes).difference(train_unlable_genes):
+        fu.write(gene)
+        fu.write("\n")
+    fu.close()
+
     args = {}
     args["train_data"] = positive_data
     args["train_opt"] = opt
